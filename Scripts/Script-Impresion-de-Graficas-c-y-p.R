7afd0c800be30fcba87390b6cf969be4900982e3
Carlos Antonio Lara Verduzco <charly_0666@hotmail.com>
2015-02-18 01:24:31 -0800

    Graficas arregladas

--- Scripts/Script-Impresion-de-Graficas-c-y-p.R
+++ Scripts/Script-Impresion-de-Graficas-c-y-p.R
@@ -2,38 +2,25 @@ plotqcc <- function(n) {
   id <- 1:n
   library(qcc)
   
-  data <- vector(mode="character", length=length(id))# Se declara un vector
-                                                            #de id longitud 
-        for(i in seq_along(id)) { # Se inicia un ciclo para crear un ciclo para 
-                              # imprimir id documentos
-             x <- id[i]
-            id_string <- toString(x)
-                  
-              if (x >= 1 && x <= 9) {  
-            
-            monitor <- paste("00", id_string, ".csv", sep="")
-              
-             	}
-  
-            
-            else if (x >= 10 && x <= 99) {
-                 
-                    id_string <- toString(x)
-                
-                    monitor <- paste("0", id_string, ".csv", sep=""
-            }
-  
-            
-            else {
-    
-          id_string <- toString(x)
-
-          monitor <- paste(id_string, ".csv", sep="") 
-  }
-  data[i] <- monitor      
-  #print(monitor)
-
-
+  data <- vector(mode="character", length=length(id))# Se declara un vector de id longitud
+                                                             
+        for(i in seq_along(id)) {
+                x <- id[i]
+                id_string <- toString(x)
+                if (x >= 1 && x <= 9) {  
+                        monitor <- paste("00", id_string, ".csv", sep="")
+                }
+                else if (x >= 10 && x <= 99) {
+                        id_string <- toString(x)
+                        monitor <- paste("0", id_string, ".csv", sep="")
+                }
+                else {
+                        id_string <- toString(x)
+                        monitor <- paste(id_string, ".csv", sep="") 
+                }
+                data[i] <- monitor      
+                #print(monitor)
+        }
 	library(qcc)
 
   for(i in data) {
